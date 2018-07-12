2084e7d275a4fb321265daccc72a5f6b9c8ba2ce
cdevries546 <cdevries@comcast.net>
2018-07-12 19:51:34 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-// Code your solutions in this file
+// var gifts =['drone', 'Jordans', 'clothes', 'corgi', 'books','pomsky']
+// function wrapGift(gifts) {
+//   console.log(gifts+ "has been wrapped")
+// }
+// for (var counter = 0; counter < gifts.length; counter++){
+//   var gift =gifts[counter]
+//   wrapGift(gift)
+
+function printBadges(array){
+  for(let i=0; i< array.length; i++){
+  console.log("Welcome " + array[i] + "! You are a employee # " + (i=1)+ ".")
+}
+return array
+}
